9d4807da6d39264d661c5ee4ee7a31722ad00b99
fala669 <falla1234hd@hotmail.com>
2024-07-11 15:45:16 -0500

    Trabajo corregido

--- ejercicio5.php
+++ ejercicio5.php
@@ -1,101 +1,72 @@
 <?php
 
-echo "Ingrese cantidad de empleados \n";
-$cantidad_empleados = intval(readline());
-$empleados = []; 
 
 
-for ($i = 1; $i <= $cantidad_empleados; $i++) {
-    echo "Empleado $i\n";
-    echo "Ingrese su nombre\n";
-    $nombre = readline();
 
-    echo "Ingrese su Identidad \n ";
-    $dni = intval(readline());
-    echo "Ingrese su género \n";
-    $genero = readline();
-    echo "Ingrese su edad \n";
-    $edad = intval(readline());
-    echo "Ingrese su estatura \n";
-    $estatura = floatval(readline());
-    echo "Ingrese su peso \n";
-    $peso = floatval(readline());
+echo"ingrese Nombre\n";
+$nombre = readline();
+echo "Ingrese contraseña\n";
+$contraseña = readline();
 
-    $empleados[] = [
-        'nombre' => $nombre,
-        'dni' => $dni,
-        'genero' => $genero,
-        'edad' => $edad,
-        'estatura' => $estatura,
-        'peso' => $peso,
-    ];
-
-    echo "\n";
-}
-
-echo "NOMBRE DEL EMPLEADO \n";
-$nombre_buscar = readline();
-echo "IDENTIDAD DEL EMPLEADO \n";
-$dni_buscar = intval(readline());
-
-$encontrado = false;
-
-foreach ($empleados as $empleado) {
-    if ($empleado['nombre'] === $nombre_buscar && $empleado['dni'] === $dni_buscar) {
-        echo "EMPLEADO:\n";
-        echo $empleado;
-        $encontrado = true;
-        break; 
+if ($nombre == "alejandro" && $contraseña = 1234) {
+    echo "Ingrese cantidad de empleados \n";
+    $cantidadempleados = intval(readline());
+    $empleados = []; 
+    
+    
+    for ($i = 1; $i <= $cantidadempleados; $i++) {
+        echo "Empleado $i\n";
+        echo "Ingrese su nombre\n";
+        $nombre = readline();
+    
+        echo "Ingrese su Identidad \n ";
+        $dni = intval(readline());
+        echo "Ingrese su género \n";
+        $genero = readline();
+        echo "Ingrese su edad \n";
+        $edad = intval(readline());
+        echo "Ingrese su estatura \n";
+        $estatura = floatval(readline());
+        echo "Ingrese su peso \n";
+        $peso = floatval(readline());
+    
+        $empleados[] = [
+            'nombre' => $nombre,
+            'dni' => $dni,
+            'genero' => $genero,
+            'edad' => $edad,
+            'estatura' => $estatura,
+            'peso' => $peso,
+        ];
+    
+        echo "\n";
     }
-}
+    
+    echo "NOMBRE DEL EMPLEADO \n";
+    $nombrebuscar = readline();
+    echo "IDENTIDAD DEL EMPLEADO \n";
+    $dnibuscar = intval(readline());
+    
 
-if (!$encontrado) {
-    echo "EMPLEADO NO REGISTRADO\n";
+    
+    foreach ($empleados as $empleado) {
+        if ($empleado['nombre'] === $nombrebuscar && $empleado['dni'] === $dnibuscar) {
+            echo "EMPLEADO:\n";
+            echo $empleado;
+            
+            break; 
+        }else {
+            echo"EMPLEADO NO ENCONTRADO\n";
+        }
+    }
+    
+ 
+    
+    }else {
+        echo "Nombre o Contraseña Incorrectos\n";
+    }
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
+   
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
