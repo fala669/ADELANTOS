b20d9bd3972b72dfc07e359ea11dc4f517b74dd2
fala669 <falla1234hd@hotmail.com>
2024-07-25 09:10:24 -0500

    Ejercicio funciones

---
+++ calcular.php
@@ -0,0 +1,21 @@
+<?php 
+
+function calcularDescuento(){}
+
+
+function multiplica($precioOriginal,$descuentoPorcentaje){
+$descuento = $precioOriginal * $porcentaje/100;
+$precioPagar =$precioOriginal - $descuento;
+
+return ;$precioPagar;
+}
+
+function mostrarDescuento(){
+    echo "El precio a pagar es:" . CalcularDescuento(60000,30);
+}
+
+echo "mostrar descuento" . calcularDescuento(60000,30);
+
+
+
+?>
